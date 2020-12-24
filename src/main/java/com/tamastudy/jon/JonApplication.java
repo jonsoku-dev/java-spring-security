ff125e5d78bc94d11813eec6bea52d54c10b8a4b
jonsoku2 <whdtjr2792@gmail.com>
2020-12-24 19:45:32 +0900

    init project

---
+++ src/main/java/com/tamastudy/jon/JonApplication.java
@@ -0,0 +1,13 @@
+package com.tamastudy.jon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tamastudy/jon/JonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamastudy.jon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
