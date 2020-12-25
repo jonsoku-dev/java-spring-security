8584099955d3c4030907f6651c44e3bc34f49237
jonsoku2 <whdtjr2792@gmail.com>
2020-12-25 09:56:19 +0900

    구글 로그인 준비

--- src/main/java/com/tamastudy/jon/config/SecurityConfig.java
+++ src/main/java/com/tamastudy/jon/config/SecurityConfig.java
@@ -32,6 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/loginForm")
 //                .usernameParameter("username2") // username 을 변경하고싶을 때
                 .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm"); // 구글 로그인이 완료된 후의 후처리가 필요함 (왜냐하면 구글로그인이 되도 세션이 없으니)
     }
 }
