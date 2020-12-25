619fdb587f6cf20f8bdfaf5c5c8c121ffeba6f02
jonsoku2 <whdtjr2792@gmail.com>
2020-12-25 14:13:14 +0900

    네이버 로그인 완료 !!!!!!!! 스프링 기본 로그인 + OAuth2.0 로그인 = 통합해서 구현

--- src/main/java/com/tamastudy/jon/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/tamastudy/jon/config/oauth/PrincipalOauth2UserService.java
@@ -3,6 +3,7 @@ package com.tamastudy.jon.config.oauth;
 import com.tamastudy.jon.config.auth.PrincipalDetails;
 import com.tamastudy.jon.config.oauth.provider.FacebookUserInfo;
 import com.tamastudy.jon.config.oauth.provider.GoogleUserInfo;
+import com.tamastudy.jon.config.oauth.provider.NaverUserInfo;
 import com.tamastudy.jon.config.oauth.provider.OAuth2UserInfo;
 import com.tamastudy.jon.entity.User;
 import com.tamastudy.jon.repository.UserRepository;
@@ -12,6 +13,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -38,11 +41,20 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
         OAuth2UserInfo oAuth2UserInfo = null;
         if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            System.out.println("구글 로그인 요청");
             oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+            System.out.println("페이스북 로그인 요청");
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            System.out.println("네이버 로그인 요청");
+            /**
+             * response 안에 response 가 있기 때문에
+             * response = {resultcode=00, message=success, response={id=69671482, nickname=공뇽, profile_image=https://ssl.pstatic.net/static/pwe/address/img_profile.png, email=whdtjr2792@naver.com, name=이종석}}
+             */
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
         } else {
-            System.out.println("우리는 구글과 페이스북만 지원해요 !");
+            System.out.println("우리는 구글, 페이스북, 네이버만 지원해요 !");
         }
 
 //        String provider = userRequest.getClientRegistration().getRegistrationId(); // provider = google
